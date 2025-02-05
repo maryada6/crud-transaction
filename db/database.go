f2cd724d4ed7d8e8beaa25f50ba045bf615c41cb
maryada-lodha-5006976 <maryada.kumar@shopup.org>
2025-02-05 21:50:55 +0530

    fix(db): add singleton pattern

--- db/database.go
+++ db/database.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"log"
+	"sync"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,25 +12,30 @@ import (
 	"crud-transaction/models"
 )
 
-var DB *gorm.DB
+var (
+	DB   *gorm.DB
+	once sync.Once
+)
 
 func InitDB() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
-		config.GetDatabaseHost(),
-		config.GetDatabaseUser(),
-		config.GetDatabasePassword(),
-		config.GetDatabaseName(),
-		config.GetDatabasePort())
+	once.Do(func() {
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+			config.GetDatabaseHost(),
+			config.GetDatabaseUser(),
+			config.GetDatabasePassword(),
+			config.GetDatabaseName(),
+			config.GetDatabasePort())
 
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
+		var err error
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Fatalf("Failed to connect to database: %v", err)
+		}
 
-	err = DB.AutoMigrate(&models.Transaction{})
-	if err != nil {
-		log.Fatalf("Failed to apply database migrations: %v", err)
-	}
-	fmt.Println("Database connected successfully and migrations applied")
+		err = DB.AutoMigrate(&models.Transaction{})
+		if err != nil {
+			log.Fatalf("Failed to apply database migrations: %v", err)
+		}
+		fmt.Println("Database connected successfully and migrations applied")
+	})
 }
