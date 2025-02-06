72ccc1701d42931722faa7e7645176069ae645bd
maryada-lodha-5006976 <maryada.kumar@shopup.org>
2025-02-06 17:25:06 +0530

    fix(set): Make function private

--- db/database_test.go
+++ db/database_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestInitDB(t *testing.T) {
-	t.Run("should connect to database", func(t *testing.T) {
+	t.Run("should initialise database connection", func(t *testing.T) {
 		db = nil
 		GetDB()
 		assert.NotNil(t, db)
