4695b0769babcea419c4b020fb9d9cb99dc14d86
maryada-lodha-5006976 <maryada.kumar@shopup.org>
2025-02-06 18:08:22 +0530

    feat(server): Gracefull shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,46 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"crud-transaction/config"
 	routes "crud-transaction/router"
 )
 
 func main() {
-	log.Println("Starting Transaction Service API")
 	router := routes.SetupRouter()
 	port := config.GetServerPort()
-	log.Println("Server is running on port", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: router,
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("Server is running on port %d", port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed: %v", err)
+		}
+	}()
+
+	<-stop
+	log.Println("Shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server gracefully stopped")
 }
